60801958fc3bb24c77affc4f78c5774fe93af22e
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-06-07 20:59:22 -0600

    Error Page and Register Page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,7 +4,15 @@ import img from '../assets/images/not-found.svg'
 import Wrapper from '../assets/wrappers/ErrorPage'
 function Error() {
     return (
-        <Wrapper className='full-page'>Router</Wrapper>
+        <Wrapper className='full-page'>
+            <div>
+                <img src={img} alt="not found" />
+                <h3>Page Not Found</h3>
+                <p>Can't find the page you're looking for at the moment. Please check back later!</p>
+                <Link to='/'>back home</Link>
+            </div>
+
+        </Wrapper>
     )
 }
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+}
 
 function Register() {
+    const [values, setValues] = useState(initialState)
+
+    const handleChance = (e) => {
+        console.log(e.target)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target)
+    }
+
     return (
         <div>Register</div>
     )
