24bd879e7ba73c6ad545132671282906b3ca1d58
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-06-21 20:11:23 -0600

    Incorporate Redux Toolkit

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit/dist/configureStore";
+import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/user/userSlice";
 
 export const store = configureStore({
