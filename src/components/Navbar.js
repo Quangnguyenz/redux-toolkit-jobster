48377f236eb18a3b56bb3f9c1c49819cdc86c4c7
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-08-01 19:59:27 -0700

    Dashboard

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,8 +14,24 @@ const Navbar = () => {
                 <button type='button' className='toggle-btn' onClick={() => console.log('toggle sidebar')}>
                     <FaAlignLeft />
                 </button>
+                <div>
+                    <Logo />
+                    <h3 className='logo-text'>dashboard</h3>
+                </div>
+                <div className="btn-container">
+                    <button type='button' className='btn' onClick={console.log('toggle logout dropdown')}>
+                        <FaUserCircle></FaUserCircle>
+                        {user?.name}
+                        <FaCaretDown />
+                    </button>
+                    <div className="dropdown show-dropdown">
+                        <button type='button' className='dropdown-btn' onClick={console.log('logout user')}>
+                            logout
+                        </button>
+                    </div>
+                </div>
             </div>
-        </Wrapper>
+        </Wrapper >
     )
 }
 
