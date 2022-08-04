8f9dadef34d21c63960ecb475591265ce7cb0796
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-08-03 20:31:34 -0700

    Dashboard

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { toggleSidebar } from '../features/user/userSlice';
 const Navbar = () => {
+    const [showLogout, setShowLogout] = useState(false)
     const user = useSelector((store) => store.user)
     const dispatch = useDispatch()
 
@@ -25,12 +26,12 @@ const Navbar = () => {
                     <h3 className='logo-text'>dashboard</h3>
                 </div>
                 <div className="btn-container">
-                    <button type='button' className='btn' onClick={console.log('toggle logout dropdown')}>
+                    <button type='button' className='btn' onClick={() => setShowLogout(!showLogout)}>
                         <FaUserCircle></FaUserCircle>
                         {user?.name}
                         <FaCaretDown />
                     </button>
-                    <div className="dropdown show-dropdown">
+                    <div className={showLogout ? "dropdown show-dropdown" : 'dropdown'}>
                         <button type='button' className='dropdown-btn' onClick={console.log('logout user')}>
                             logout
                         </button>
