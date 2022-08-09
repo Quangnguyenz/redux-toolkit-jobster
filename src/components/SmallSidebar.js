ad67c294c1717886d327193293a1d6618e5103ae
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-08-08 20:29:47 -0700

    Dashboard

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
+import { useSelector, useDispatch } from 'react-redux';
 
 const SmallSidebar = () => {
     return (
         <Wrapper>
-            <h2>SmallSidebar</h2>
+            <div className="sidebar-container show-sidebar">
+                <div className="content">
+                    <button className='close-btn' onClick={() => console.log('toggle')}>
+                        <FaTimes />
+                    </button>
+                    <header>
+                        <Logo />
+                    </header>
+                    <div className="nav-links">
+                        nav link
+                    </div>
+                </div>
+            </div>
         </Wrapper>
     )
 }
