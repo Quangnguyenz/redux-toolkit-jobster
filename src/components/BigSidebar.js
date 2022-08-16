0e6f9109112fca6fbee05d7bc0137579ac998f9d
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-08-15 20:14:48 -0700

    Dashboard

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Logo from '../components/Logo'
 import Wrapper from '../assets/wrappers/BigSidebar';
+import { useSelector } from 'react-redux';
+import NavLinks from './NavLinks';
 
 const BigSidebar = () => {
+    const { isSidebarOpen } = useSelector((store) => store.user)
     return (
         <Wrapper>
-            <div>BigSidebar</div>
+            <div className={isSidebarOpen ? 'sidebar-container ' : 'sidebar-container show-sidebar'}>
+                <div className="content">
+                    <header>
+                        <Logo />
+                    </header>
+                    <NavLinks />
+                </div>
+            </div>
         </Wrapper>
     )
 }
