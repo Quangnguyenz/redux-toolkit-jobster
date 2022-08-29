85b3f64414b10b9f9edcafd5e3effabb00cb5990
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-08-28 19:59:26 -0700

    Profile

---
+++ src/features/user/userThunk.js
@@ -0,0 +1,11 @@
+import customFetch from '../../utils/axios'
+import { logoutUser } from './userSlice'
+
+export const registerUserThunk = async (url, user, thunkAPI) => {
+    try {
+        const resp = await customFetch.post(url, user)
+        return resp.data
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
+}
