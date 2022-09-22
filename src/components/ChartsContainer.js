7e1d14e59595080c8b20ba2da5b4cfc487766a85
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-09-21 19:05:20 -0700

    Job Stats

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import React, { useState } from 'react';
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+import { useSelector } from 'react-redux';
+
 const ChartsContainer = () => {
+    const [BarChart, setBarChart] = useState(true)
+    const { monthlyApplication: data } = useSelector((store) => store.allJobs)
     return (
-        <div>ChartsContainer</div>
+        <Wrapper>
+            <h4>Monthhly Applications</h4>
+            <button type='button' onClick={() => setBarChart(!barChart)}>
+                {barChart}
+            </button>
+        </Wrapper>
     )
 }
 
